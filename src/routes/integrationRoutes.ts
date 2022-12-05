79adf0941d7f7ae79037be8fc56ad19c90a2c456
Rok Pusnik <7842426+push989@users.noreply.github.com>
2022-12-05 11:57:46 +0100

    Add endpoint for getting the logs stream

--- src/routes/integrationRoutes.ts
+++ src/routes/integrationRoutes.ts
@@ -1,16 +1,25 @@
 import Router from "express-promise-router";
-import MainService from "../services/IntegratorService";
+import IntegratorService from "../services/IntegratorService";
+import fs from "fs";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
   const syncOptions = req.body;
   try {
-    await MainService.integrate(syncOptions);
+    await IntegratorService.integrate(syncOptions);
     res.send("OK");
   } catch (error) {
     res.status(500).send({ error });
   }
+});
+
+router.get("/logs", async (req, res) => {
+  const readStream = fs.createReadStream("./logs");
+  res.writeHead(200, { "Content-Type": "application/ndjson" });
+  readStream.on("open", () => {
+    readStream.pipe(res);
+  });
 });
 
 export default router;
